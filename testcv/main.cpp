2835e0c34b3b4176cb449d8a43a31081cd58347a
zzb66666666x <18767066589@163.com>
2020-08-06 17:09:41 +0800

    updated

--- testcv/main.cpp
+++ testcv/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
  {
-     char s[] = "Hi,Cpp.";
+    char s[] = "Hi,Cpp.";
     int n = strlen(s);
     printf("%s:%d\n",s,n);
 
     cout << "Hello Opencv4.2" << endl;
     cout << cv::COLOR_RGB2BGRA << cv::COLOR_RGB2GRAY << endl;
-    cv::Mat a = cv::imread("C:/Users/zzb/Pictures/Saved Pictures/bilibili.jpg");
-   cv::imshow("hello",a);
+    cv::Mat a = cv::imread("bilibili.jpg");
+    cv::imshow("2233",a);
     cv::waitKey(0);
     return 0;
  }
