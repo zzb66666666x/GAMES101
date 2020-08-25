93f212d23031b941f374f02be697e1b390184b3a
zzb66666666x <18767066589@163.com>
2020-08-25 16:25:40 +0800

    updated

---
+++ Assignment4/build/CMakeFiles/BezierCurve.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Mycode/Cpp/GAMES101/Assignment4/main.cpp" "D:/Mycode/Cpp/GAMES101/Assignment4/build/CMakeFiles/BezierCurve.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "D:/CodingLibs/opencv/mingw_build"
+  "D:/CodingLibs/opencv/sources/include"
+  "D:/CodingLibs/opencv/sources/modules/core/include"
+  "D:/CodingLibs/opencv/sources/modules/flann/include"
+  "D:/CodingLibs/opencv/sources/modules/imgproc/include"
+  "D:/CodingLibs/opencv/sources/modules/ml/include"
+  "D:/CodingLibs/opencv/sources/modules/photo/include"
+  "D:/CodingLibs/opencv/sources/modules/dnn/include"
+  "D:/CodingLibs/opencv/sources/modules/features2d/include"
+  "D:/CodingLibs/opencv/sources/modules/imgcodecs/include"
+  "D:/CodingLibs/opencv/sources/modules/videoio/include"
+  "D:/CodingLibs/opencv/sources/modules/calib3d/include"
+  "D:/CodingLibs/opencv/sources/modules/highgui/include"
+  "D:/CodingLibs/opencv/sources/modules/objdetect/include"
+  "D:/CodingLibs/opencv/sources/modules/stitching/include"
+  "D:/CodingLibs/opencv/sources/modules/video/include"
+  "D:/CodingLibs/opencv/sources/modules/gapi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
