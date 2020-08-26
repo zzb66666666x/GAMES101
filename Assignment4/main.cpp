9cc023e1ea6b6a1145b415fa08a7318c9293d6fe
zzb66666666x <18767066589@163.com>
2020-08-27 00:28:33 +0800

    updated

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -9,6 +9,12 @@ using std::string;
 //global definition
 std::vector<cv::Point2f> control_points;
 
+typedef enum color_theme{
+    GREEN = 1,
+    RED,
+    BLUE
+} COLOR;
+
 void mouse_handler(int event, int x, int y, int flags, void *userdata) 
 {
     if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4) 
@@ -20,7 +26,16 @@ void mouse_handler(int event, int x, int y, int flags, void *userdata)
     }     
 }
 
-void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
+void fill_pixel( cv::Mat &window, cv::Point2f point, COLOR color, bool use_anti_alias){
+    if (use_anti_alias){
+        window.at<cv::Vec3b>(point.y, point.x)[color] = 255;
+    }
+    else{
+        window.at<cv::Vec3b>(point.y, point.x)[color] = 255;
+    }
+}
+
+void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window, bool use_anti_alias) 
 {
     auto &p_0 = points[0];
     auto &p_1 = points[1];
@@ -32,8 +47,7 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
         //analytic solution for 3 order Bezier Curve
         auto point = std::pow(1 - t, 3) * p_0 + 3 * t * std::pow(1 - t, 2) * p_1 +
                  3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
-
-        window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
+        fill_pixel(window, point, RED, use_anti_alias);
     }
 }
 
@@ -60,21 +74,21 @@ cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, flo
     }
 }
 
-void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
+void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window, bool use_anti_alias) 
 {
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
     double step_size = 0.001;
     for (double t=0.0; t<=1.0; t+=step_size){
         auto point = recursive_bezier(control_points, t);
-        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+        fill_pixel(window, point, GREEN, use_anti_alias);
     }
 }
 
 int main(int argc, const char ** argv) 
 {
     try{
-        if (argc>3 || argc < 1){
+        if (argc>4 || argc < 1){
             throw "invalid number of params";
         }
         cv::Mat window = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
@@ -84,6 +98,7 @@ int main(int argc, const char ** argv)
         string path = "../images/";
         string filename = "naive_bezier.png";
         bool use_naive_bezier = true;
+        bool use_anti_alias = false;
         int key = -1;
         if (argc >= 2){
             //specify mode
@@ -97,10 +112,18 @@ int main(int argc, const char ** argv)
                     throw "invalid mode";
                 }
             }
-            if(argc == 3){
-                //specify filename
-                string name = argv[2];
-                filename = name;
+            if(argc >= 3){
+                if (argv[2] == "AA_on"){
+                    use_anti_alias = true;
+                }
+                else if (argv[2] == "AA_off"){
+                    throw "invalid argument";
+                }
+                if (argc == 4){
+                    //specify filename
+                    string name = argv[3];
+                    filename = name;
+                }
             }
         }
         filename = path + filename;
@@ -111,10 +134,10 @@ int main(int argc, const char ** argv)
 
             if (control_points.size() == 4) {
                 if (use_naive_bezier){
-                    naive_bezier(control_points, window);
+                    naive_bezier(control_points, window, use_anti_alias);
                 }
                 else{
-                    bezier(control_points, window);
+                    bezier(control_points, window, use_anti_alias);
                 }
 
                 cv::imshow("Bezier Curve", window);
