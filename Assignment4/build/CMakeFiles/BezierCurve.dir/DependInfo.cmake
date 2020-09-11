8bbdce5fec9e12cabbf9c0557b1ef967ce7d9b40
zzb66666666x <18767066589@163.com>
2020-09-11 18:50:13 +0800

    updated

--- Assignment4/build/CMakeFiles/BezierCurve.dir/DependInfo.cmake
+++ Assignment4/build/CMakeFiles/BezierCurve.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/Mycode/Cpp/GAMES101/Assignment4/main.cpp" "D:/Mycode/Cpp/GAMES101/Assignment4/build/CMakeFiles/BezierCurve.dir/main.cpp.obj"
+  "D:/MyCode/Cpp/GAMES101/Assignment4/main.cpp" "D:/MyCode/Cpp/GAMES101/Assignment4/build/CMakeFiles/BezierCurve.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Assignment5/build/ppm_converter.py
+++ Assignment5/build/ppm_converter.py
@@ -1,3 +1,3 @@
 from PIL import Image
 img = Image.open('binary.ppm')
-img.save('ray_tracing_result.jpg')
+img.save('ray_tracing_result.png')
